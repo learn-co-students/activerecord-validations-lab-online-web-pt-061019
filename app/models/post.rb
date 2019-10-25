5d7ea37336f2b3e300ef410ee142f3a0962176e8
HuydDo <huydinhdo@yahoo.com>
2019-10-25 13:55:12 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,9 @@
 class Post < ActiveRecord::Base
-  # include ActiveModel::Validations
-  class ClickbaitValidator < ActiveModel::EachValidator
-    def validate_each(record, attribute, value)
-      unless value =~ /(Won't Believe|Secret|Top \d|Guess)/
-        record.errors[attribute] << ("Not a clickbait title")
-      end
-    end
-  end
-  
+  include ActiveModel::Validations
+
   validates :title, presence: true, clickbait: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
 end
+
--- app/validators/clickbait_title_validator.rb
+++
@@ -1,7 +0,0 @@
-class ClickBaitTitleValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    unless value =~ /\b(Wont't Believe|Secret|Top \d|Guess)\b/
-      record.errors[:attribute] << "Title is not clickbaity enought"
-    end  
-  end
-end
