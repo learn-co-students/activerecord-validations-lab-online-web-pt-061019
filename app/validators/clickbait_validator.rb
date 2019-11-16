dfb6035aa49ac889fd288e76809c24dc3755b8db
fasnimo <fasnimo@gmail.com>
2019-11-16 01:16:51 -0600

    Done.

---
+++ app/validators/clickbait_validator.rb
@@ -0,0 +1,7 @@
+class ClickbaitValidator < ActiveModel::Validator
+  def validate(record)
+    unless record.title =~ /\b(Won't Believe|Secret|Top \d|Guess)\b/
+        record.errors[:title] << "Title is not clickbaity enough"
+        end   
+    end
+end   
